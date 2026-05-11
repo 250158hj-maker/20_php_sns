8f116dcc6dd0e20afdd7b848bae1bcea6a17ca63
Takumi Mito <250158hj@yse-c.net>
2026-05-11 14:55:56 +0900

    fix: GETパラメータの取得方法の変更

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -30,7 +30,7 @@ class HomeController extends AuthenticatedController
     public function detail()
     {
         // TODO: GETパラメータからIDを取得: $_GET['id']
-        $id = $_GET['id'];
+        $id = $_GET['id'] ?? 0;
         // 投稿IDがなければホームにリダイレクト
         if (!$id) Request::redirect('home/');
 
