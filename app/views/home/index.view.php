f398219d05b50e45bd584e5e90266f81e5d83c71
Takumi Mito <250158hj@yse-c.net>
2026-05-11 14:00:18 +0900

    fix: コメントの削除

--- app/views/home/index.view.php
+++ app/views/home/index.view.php
@@ -1,20 +1,12 @@
 <div class="flex max-w-4xl mx-auto min-h-screen">
-
-    <!-- サイドナビ -->
     <header class="sticky top-0 h-screen w-20 shrink-0 self-start border-r border-slate-100 xl:w-56">
-        <!-- ホームタブ: app/views/components/home_tabs.php -->
         <?php include COMPONENT_DIR . 'nav.php' ?>
     </header>
 
-    <!-- メインコンテンツ -->
     <main class="flex-1 border-r border-slate-100 min-h-screen">
-        <!-- ホームタブ: app/views/components/home_tabs.php -->
         <?php include COMPONENT_DIR . 'home_tabs.php' ?>
-        <!-- TODO: 検索フォーム読み込み: app/views/components/search_form.php -->
         <?php include COMPONENT_DIR . 'search_form.php' ?>
-        <!-- TODO: ツイート投稿フォーム読み込み: app/views/components/tweet_form.php -->
         <?php include COMPONENT_DIR . 'tweet_form.php' ?>
-        <!-- ホームツイートリスト: app/views/components/home_tweet_list.php -->
         <?php include COMPONENT_DIR . 'home_tweet_list.php' ?>
     </main>
 </div>
