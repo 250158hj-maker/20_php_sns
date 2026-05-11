b7c11d012249da11f273623dfd8b792ed8f5ec60
Takumi Mito <250158hj@yse-c.net>
2026-05-11 13:41:12 +0900

    initial commit

---
+++ env.sample.php
@@ -0,0 +1,17 @@
+<?php
+// Database
+const DB_CONNECTION = '';
+const DB_HOST = '';
+const DB_PORT = '';
+const DB_DATABASE = '';
+const DB_USERNAME = '';
+const DB_PASSWORD = '';
+const DB_CHARSET = '';
+
+// アプリキー（セッションキー兼用）
+const APP_KEY = "";
+
+// サイトタイトル
+const SITE_TITLE = " ";
+
+// const BASE_URL = "http://localhost/php_sns/";
